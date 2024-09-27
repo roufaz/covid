82bf29a3298595a4441b91cb28a6d354411641c5
roufaz <162562892+roufaz@users.noreply.github.com>
2024-09-27 17:18:00 +0000

    ccch

--- covid.py
+++ covid.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 st.set_page_config(layout='wide')
-data=pd.read_csv('/workspaces/Covid_case/Covid_Dashboard1.csv')
+data=pd.read_csv('/workspaces/covid/Covid_Dashboard1.csv')
 
 
 expander=st.expander('See data')
@@ -15,16 +16,15 @@ df=data[data['State/UTs']==select]
 
 col1,col2,col3,col4=st.columns(4)
 s1=col1.metric('Total cases',df['Total Cases'].sum())
-col2.metric('Total Recoveries',df['Discharged'].sum())
+s2=col2.metric('Total Recoveries',df['Discharged'].sum())
 
 #dd=(df['Discharged'] / s1()) * 100
 
 col3.metric('Deaths',df['Deaths'].sum())
+
 col4.metric('Active',df['Active'].sum())
 
-#df1 = pd.DataFrame(columns=['State', 'Case' ])
-#df1['State'] = [value],['Case']=[s1]
-#st.write(df1)
+
 
 
 colu1,colu2=st.columns(2)
@@ -62,8 +62,66 @@ co2.dataframe(op_states)
 
 
 
-colu3,colu4=st.columns(2)
 
+colu3,colu4=st.columns(2)
 con=colu3.container(border=True)
+con.markdown("<h2 style='text-align: center;'>Recovered</h2>", unsafe_allow_html=True)
+
+c3,c4=con.columns(2)
+states1 = data.nlargest(5, 'Discharged')[['State/UTs', 'Discharged']]
+states1 = states1.reset_index(drop=True)
+c3.subheader(" Highest Cases")
+c3.dataframe(states1)
+
+states1 = data.nsmallest(5, 'Discharged')[['State/UTs', 'Discharged']]
+states1 = states1.reset_index(drop=True)
+c4.subheader("Least Cases")
+c4.dataframe(states1)
+
+con=colu4.container(border=True)
 con.markdown("<h2 style='text-align: center;'>Active</h2>", unsafe_allow_html=True)
 
+
+co3,co4=con.columns(2)
+op_states1 = data.nlargest(5, 'Active')[['State/UTs', 'Active']]
+op_states1 = op_states1.reset_index(drop=True)
+co3.subheader("Highest Cases")
+co3.dataframe(op_states1)
+
+op_states1 = data.nsmallest(5, 'Active')[['State/UTs', 'Active']]
+op_states1 = op_states1.reset_index(drop=True)
+co4.subheader("Least Cases")
+co4.dataframe(op_states1)
+
+
+
+
+
+
+colum1,colum2=st.columns(2)
+
+colum1.subheader('Death Ratio')
+#df_reversed = data.iloc[::-1]
+fig=px.bar(data,x='Death Ratio',y='State/UTs',color='Death Ratio',color_continuous_scale='Viridis')
+colum1.plotly_chart(fig)
+
+colum2.subheader('Recoverer Rate')
+#df_reversed = states1.iloc[::-1]
+bar1 = px.bar(data, y='State/UTs', x='Discharged', title='Recovery Ratio by State (%)',
+color='Death Ratio',color_continuous_scale='Plasma')
+colum2.write(bar1)
+
+
+colum3,colum4=st.columns(2)
+#fig1 = px.pie(data,values='Death Ratio', names='Zone', title='Pie Chart Example')
+#colum3.plotly_chart(fig1)
+
+colum3.subheader('Active & Death plot')
+fig2=px.bar(data,x='State/UTs',y=['Active','Deaths'],barmode='group',
+color_discrete_sequence=['#1f77b4', '#ff7f0e'])
+colum3.plotly_chart(fig2) 
+
+colum4.subheader('Cases & Recovery plot')
+fig3=px.bar(data,x='State/UTs',y=['Total Cases','Discharged'],barmode='group',
+color_discrete_sequence=['#28a745', '#dc3545'])
+colum4.plotly_chart(fig3) 
